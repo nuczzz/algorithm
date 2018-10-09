8ecbf6b3d628fd42df86d11af8b67747761d46d7
nuczzz <nuc_zzz@163.com>
2018-10-09 15:01:19 +0800

    format code

--- stack.go
+++ stack.go
@@ -1,1 +1,60 @@
 package algorithm
+
+import (
+	"errors"
+)
+
+type Stack struct {
+	maxLength int
+	elements  []interface{}
+}
+
+const (
+	defaultStackLength = 3
+)
+
+var (
+	ErrStackUnderflow = errors.New("stack underflow")
+	ErrStackOverflow  = errors.New("stack overflow")
+)
+
+func NewStack(length int) *Stack {
+	return &Stack{
+		maxLength: length,
+		elements:  make([]interface{}, 0, length),
+	}
+}
+
+func NewDefaultStack() *Stack {
+	return NewStack(defaultStackLength)
+}
+
+func (s *Stack) IsEmpty() bool {
+	return len(s.elements) == 0
+}
+
+func (s *Stack) Pop() (interface{}, error) {
+	if s.IsEmpty() {
+		return nil, ErrStackUnderflow
+	}
+
+	length := len(s.elements)
+	ret := s.elements[length-1]
+	s.elements = s.elements[:length-1]
+
+	return ret, nil
+}
+
+func (s *Stack) Push(element interface{}) error {
+	if len(s.elements) >= s.maxLength {
+		return ErrStackOverflow
+	}
+
+	s.elements = append(s.elements, element)
+
+	return nil
+}
+
+func (s *Stack) List() []interface{} {
+	return s.elements
+}
--- stack_test.go
+++ stack_test.go
@@ -1,1 +1,34 @@
 package algorithm
+
+import "testing"
+
+const stackLength = 3
+
+func TestNewDefaultStack(t *testing.T) {
+	stack := NewDefaultStack()
+	_, err := stack.Pop()
+	if err == nil {
+		t.Fatal("should underflow")
+	}
+	t.Log(err)
+
+	for i := 0; i < stackLength; i++ {
+		err := stack.Push(i)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	list := stack.List()
+	for ele := range list {
+		t.Log(ele)
+	}
+
+	err = stack.Push(11)
+	if err == nil {
+		t.Fatal("should overflow")
+	}
+	t.Log(err)
+
+	t.Log("TestNewDefaultStack success")
+}
