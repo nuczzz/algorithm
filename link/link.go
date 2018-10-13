7a76c85bb843263c352b75dbd618a8ad42432b67
nuczzz <nuc_zzz@163.com>
2018-10-13 23:29:13 +0800

    link reverse

--- link/link.go
+++ link/link.go
@@ -5,9 +5,60 @@ type LinkNode struct {
 	Next *LinkNode
 }
 
+type LinkLog interface {
+	Printf(string, ...interface{})
+}
+
 func NewLinkNode(data interface{}) *LinkNode {
 	return &LinkNode{
 		Data: data,
 		Next: nil,
 	}
+}
+
+func TraversalLink(head *LinkNode, log LinkLog) {
+	node := head
+	for node != nil {
+		log.Printf("%v\n", node.Data)
+		node = node.Next
+	}
+}
+
+func InitTestLink(num int) *LinkNode {
+	var head, tail *LinkNode
+	for i := 0; i < num; i++ {
+		node := NewLinkNode(i)
+
+		if i == 0 {
+			head = node
+			tail = node
+		} else {
+			tail.Next = node
+			tail = node
+		}
+	}
+
+	return head
+}
+
+func ReverseLink(head *LinkNode) *LinkNode {
+	// null or one link node
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	// two or more link node
+	var n1 *LinkNode
+	n2 := head
+	n3 := head.Next
+	for n3 != nil{
+		n2.Next = n1
+
+		n1 = n2
+		n2 = n3
+		n3 = n3.Next
+	}
+
+	n2.Next = n1
+	return n2
 }
--- link/link_test.go
+++ link/link_test.go
@@ -1,35 +1,65 @@
 package link
 
-import "testing"
+import (
+	"testing"
+)
 
-func TraversalLink(head *LinkNode, t *testing.T) {
-	node := head
-	for node != nil {
-		t.Logf("%v", node.Data)
-		node = node.Next
+type TestingLog struct {
+	t *testing.T
+}
+
+func NewTestingLog(t *testing.T) LinkLog {
+	return &TestingLog{
+		t: t,
 	}
 }
 
-func InitLink() *LinkNode {
-	data := []int{2, 1, 4, 3, 5}
-
-	var head, tail *LinkNode
-	for i, d := range data {
-		node := NewLinkNode(d)
-
-		if i == 0 {
-			head = node
-			tail = node
-		} else {
-			tail.Next = node
-			tail = node
-		}
-	}
-
-	return head
+func (tl *TestingLog) Printf(format string, data...interface{}){
+	tl.t.Logf(format, data)
 }
 
 func TestLink(t *testing.T) {
-	head := InitLink()
-	TraversalLink(head, t)
+	log := NewTestingLog(t)
+
+	head := InitTestLink(3)
+	TraversalLink(head, log)
+}
+
+func TestReverseLink(t *testing.T) {
+	log := NewTestingLog(t)
+
+	head := InitTestLink(0)
+	t.Log("zero node link traversal:")
+	TraversalLink(head, log)
+	t.Log("zero node link reverse:")
+	head = ReverseLink(head)
+	TraversalLink(head, log)
+
+	head = InitTestLink(1)
+	t.Log("one node link traversal:")
+	TraversalLink(head, log)
+	t.Log("one node link reverse:")
+	head = ReverseLink(head)
+	TraversalLink(head, log)
+
+	head = InitTestLink(2)
+	t.Log("two node link traversal:")
+	TraversalLink(head, log)
+	t.Log("two node link reverse:")
+	head = ReverseLink(head)
+	TraversalLink(head, log)
+
+	head = InitTestLink(3)
+	t.Log("three node link traversal:")
+	TraversalLink(head, log)
+	t.Log("three node link reverse:")
+	head = ReverseLink(head)
+	TraversalLink(head, log)
+
+	head = InitTestLink(10)
+	t.Log("ten node link traversal:")
+	TraversalLink(head, log)
+	t.Log("ten node link reverse:")
+	head = ReverseLink(head)
+	TraversalLink(head, log)
 }
