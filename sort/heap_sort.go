d7ff19d45d037fd7abe63ae7520a10edc9919cde
nuczzz <nuc_zzz@163.com>
2018-10-25 23:30:20 +0800

    optimization of heap sort

--- sort/heap_sort.go
+++ sort/heap_sort.go
@@ -1,8 +1,8 @@
 package sort
 
 func HeapSortSink(data SortInterface, i, length int) {
-	left := i<<1+1
-	right := i<<1+2
+	left := i<<1 + 1
+	right := i<<1 + 2
 
 	var largest int
 	if left < length && data.Less(i, left) {
@@ -21,20 +21,16 @@ func HeapSortSink(data SortInterface, i, length int) {
 }
 
 func buildHeapSort(data SortInterface) {
-	for i := data.Len()/2-1; i >= 0; i-- {
+	for i := data.Len()/2 - 1; i >= 0; i-- {
 		HeapSortSink(data, i, data.Len())
 	}
 }
 
 func HeapSort(data SortInterface) {
-	if data.Len() > 1 {
-		buildHeapSort(data)
+	buildHeapSort(data)
 
-		length := data.Len()
-		for i := data.Len()-1; i > 0; i-- {
-			data.Swap(i, 0)
-			length--
-			HeapSortSink(data, 0, length)
-		}
+	for i := data.Len() - 1; i > 0; i-- {
+		data.Swap(i, 0)
+		HeapSortSink(data, 0, i)
 	}
 }
