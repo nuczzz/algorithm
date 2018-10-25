2ae05a91dec164eeeff7c745a476863f4c9d9593
nuczzz <nuc_zzz@163.com>
2018-10-25 23:34:15 +0800

    optimization

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -1,37 +1,33 @@
 package sort
 
 func BubbleSort(data SortInterface) {
-	if data.Len() > 1 {
-		for i := 0; i < data.Len(); i++ {
-			for j := 1; j < data.Len()-i; j++ {
-				if !data.Less(j-1, j) {
-					data.Swap(j-1, j)
-				}
+	for i := 0; i < data.Len(); i++ {
+		for j := 1; j < data.Len()-i; j++ {
+			if !data.Less(j-1, j) {
+				data.Swap(j-1, j)
 			}
 		}
 	}
 }
 
 func CocktailSort(data SortInterface) {
-	if data.Len() > 1 {
-		left := 0
-		right := data.Len() - 1
-		for left < right {
-			//right forward sort
-			for i := left; i < right; i++ {
-				if !data.Less(i, i+1) {
-					data.Swap(i, i+1)
-				}
+	left := 0
+	right := data.Len() - 1
+	for left < right {
+		//right forward sort
+		for i := left; i < right; i++ {
+			if !data.Less(i, i+1) {
+				data.Swap(i, i+1)
 			}
-			right--
-
-			//left forward sort
-			for j := right; j > left; j-- {
-				if data.Less(j, j-1) {
-					data.Swap(j, j-1)
-				}
-			}
-			left++
 		}
+		right--
+
+		//left forward sort
+		for j := right; j > left; j-- {
+			if data.Less(j, j-1) {
+				data.Swap(j, j-1)
+			}
+		}
+		left++
 	}
 }
--- sort/insertion_sort.go
+++ sort/insertion_sort.go
@@ -1,41 +1,37 @@
 package sort
 
 func InsertionSort(data SortInterface) {
-	if data.Len() > 1 {
-		for j := 1; j < data.Len(); j++ {
-			for i := j - 1; i >= 0; i-- {
-				if !data.Less(i, i+1) {
-					data.Swap(i, i+1)
-				}
+	for j := 1; j < data.Len(); j++ {
+		for i := j - 1; i >= 0; i-- {
+			if !data.Less(i, i+1) {
+				data.Swap(i, i+1)
 			}
 		}
 	}
 }
 
 func BinaryInsertionSort(data SortInterface) {
-	if data.Len() > 1 {
-		for j := 1; j < data.Len(); j++ {
-			left, middle, right := 0, 0, j-1
-			for left < right {
-				middle = (left + right) / 2
-				if data.Less(middle, j) {
-					left = middle+1
-				} else {
-					right = middle-1
-				}
+	for j := 1; j < data.Len(); j++ {
+		left, middle, right := 0, 0, j-1
+		for left < right {
+			middle = (left + right) / 2
+			if data.Less(middle, j) {
+				left = middle + 1
+			} else {
+				right = middle - 1
 			}
+		}
 
-			for i := j; i > left; i-- {
-				if data.Less(i, i-1) {
-					data.Swap(i, i-1)
-				}
+		for i := j; i > left; i-- {
+			if data.Less(i, i-1) {
+				data.Swap(i, i-1)
 			}
 		}
 	}
 }
 
 func ShellSort(data SortInterface) {
-	for gap := data.Len()/2; gap > 0; gap/=2 {
+	for gap := data.Len() / 2; gap > 0; gap /= 2 {
 		for i := gap; i < data.Len(); i++ {
 			for j := i; j-gap >= 0; j -= gap {
 				if data.Less(j, j-gap) {
