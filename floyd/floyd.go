60fc8b4dfaf4b8a64fd480c0fec903f70e01d6d0
nuczzz <nuc_zzz@163.com>
2019-03-19 18:14:10 +0800

    update floyd algorithm

--- floyd/floyd.go
+++ floyd/floyd.go
@@ -1,6 +1,15 @@
 package floyd
 
-func Floyd(srcMatrix [][]int) (pathMatrix [][]int) {
-	//todo
-	return nil
+// matrix MUST be N * N array, and element of matrix MUST
+// greater than 0
+func Floyd(matrix [][]int) {
+	for k := 0; k < len(matrix); k++ {
+		for i := 0; i < len(matrix[k]); i++ {
+			for j := 0; j < len(matrix[k]); j++ {
+				if (matrix[i][k] + matrix[k][j]) < matrix[i][j] {
+					matrix[i][j] = matrix[i][k] + matrix[k][j]
+				}
+			}
+		}
+	}
 }
--- floyd/floyd_test.go
+++ floyd/floyd_test.go
@@ -1,25 +1,33 @@
 package floyd
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
-func TestFloyd(t *testing.T) {
-	const Infinity = -1
-	matrix := [][]int{
-		{Infinity, 12, Infinity, Infinity, Infinity, 16, 14},
-		{12, Infinity, 10, Infinity, Infinity, 7, Infinity},
-		{Infinity, 10, Infinity, 3, 5, 6, Infinity},
-		{Infinity, Infinity, 3, Infinity, 4, Infinity, Infinity},
-		{Infinity, Infinity, 5, 4, Infinity, 2, 8},
-		{16, 7, 6, Infinity, 2, Infinity, 9},
-		{14, Infinity, Infinity, Infinity, 8, 9, Infinity},
-	}
-	for i := 0; i < 7; i++ {
-		for j := 0; j < 7; j++ {
-			fmt.Printf("%d\t", matrix[i][j])
+func printMatrix(matrix [][]int) {
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			fmt.Printf("%5d\t", matrix[i][j])
 		}
 		fmt.Println()
 	}
+}
+
+func TestFloyd(t *testing.T) {
+	const Infinity = 10000
+	matrix := [][]int{
+		{0, 12, Infinity, Infinity, Infinity, 16, 14},
+		{12, 0, 10, Infinity, Infinity, 7, Infinity},
+		{Infinity, 10, 0, 3, 5, 6, Infinity},
+		{Infinity, Infinity, 3, 0, 4, Infinity, Infinity},
+		{Infinity, Infinity, 5, 4, 0, 2, 8},
+		{16, 7, 6, Infinity, 2, 0, 9},
+		{14, Infinity, Infinity, Infinity, 8, 9, 0},
+	}
+	fmt.Println("before:")
+	printMatrix(matrix)
+	Floyd(matrix)
+	fmt.Println("after:")
+	printMatrix(matrix)
 }
